f2dc4abc54c9af5c13566ec11c20393049e57e45
Shields <mshields@nrel.gov>
2023-07-05 20:03:50 -0600

    Update plots

--- Complete_code/figures.py
+++ Complete_code/figures.py
@@ -12,8 +12,8 @@ import numpy as np
 def plot_emissions(sn, df_name):
 
     df = pd.read_excel(df_name, sheet_name = sn)
-
-    x_ind = np.arange(0, 10, 1)
+    
+    x_ind = np.arange(0, len(df), 1)
 
     fig = plt.figure(figsize=(9, 6))
     ax1 = fig.add_subplot(111)
@@ -28,7 +28,7 @@ def plot_emissions(sn, df_name):
     inst_abs = np.array(df['Installation absolute']/1000000)
     om_abs = np.array(df['O&M absolute']/1000000)
     ct_abs = np.array(df['CT absolute']/1000000)
-
+    
     ax1.bar(x_ind-(width/2), inst_int, color='#117864', width=width)
     ax1.bar(x_ind-(width/2), om_int, bottom=inst_int, color='#1ABC9C', width=width)
     ax1.bar(x_ind-(width/2), ct_int, bottom=om_int+inst_int, color='#A3E4D7', width=width)
@@ -45,13 +45,13 @@ def plot_emissions(sn, df_name):
 
     ax1.set_title('Total emissions across all phases', weight='bold')
 
-    ax1.set_xlabel('Scenario', weight='bold')
+    ax1.set_xlabel('Deployment Scenario', weight='bold')
     ax1.set_xticks(x_ind)
-    ax1.set_xticklabels(df['Scenario name'], rotation=45)
+    ax1.set_xticklabels(df['Scenario name'])
 
     handles = [
         Patch(facecolor=color, label=label)
-        for label, color in zip(['Intensity: installation', 'Intensity: O&M', 'Intensity: Component transportation', 'Emissions: Installation', 'Emissions: O&M', 'Emissions: Component transportation'], ['#117864', '#1ABC9C', '#A3E4D7', '#9C640C', '#F39C12', '#FAD7A0'])
+        for label, color in zip(['Intensity: Installation', 'Intensity: O&M', 'Intensity: Component transportation', 'Emissions: Installation', 'Emissions: O&M', 'Emissions: Component transportation'], ['#117864', '#1ABC9C', '#A3E4D7', '#9C640C', '#F39C12', '#FAD7A0'])
         ]
 
     ax1.legend(handles=handles, loc='upper left')
@@ -69,7 +69,7 @@ def activity_plot(sn, df_name):
     plt.ylabel('Million metric tons of CO2', weight='bold')
     plt.legend(loc = 'upper left')
 
-    ax.set_xticklabels(df['Scenario name'], rotation=45)
+    ax.set_xticklabels(df['Scenario name'])
     ax.set_title('Absolute emissions by activity', weight='bold')
     ax.set_xlabel('Scenario', weight='bold')
 
@@ -78,7 +78,7 @@ def activity_plot(sn, df_name):
     savefig = savedir + 'emissions-by-activity.png'
     fig.savefig(savefig, bbox_inches='tight', dpi=300)
 
-sn = 'summary_rename'
+sn = 'summary'
 df_name = 'scenarios_template.xlsx'
 plot_emissions(sn, df_name)
 activity_plot(sn, df_name)
